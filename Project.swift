178f5048debb4551c5d294a55ffc1133a565d001
최예주 <choi.yeaju@cashwalk.io>
2023-01-19 17:22:24 +0900

    Chore project init setting with tuist

---
+++ Project.swift
@@ -0,0 +1,52 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import MyPlugin
+
+// Local plugin loaded
+let localHelper = LocalHelper(name: "MyPlugin")
+
+// Creates our project using a helper function defined in ProjectDescriptionHelpers
+let target = Target(name: "CoffeeOverflow",
+                    platform: .iOS,
+                    product: .app,
+                    productName: "CoffeeOverflow",
+                    bundleId: "com.CoffeeOverflow",
+                    deploymentTarget: .iOS( targetVersion: "16.2", devices: .iphone),
+                    infoPlist: .file(path: "CoffeeOverflow/Info.plist"),
+                    sources: "CoffeeOverflow/Sources/**",
+                    resources: "CoffeeOverflow/Resources/**",
+                    dependencies: [],
+                    settings: .settings(configurations: [
+                        .debug(
+                            name: "Debug",
+                            settings: SettingsDictionary()
+                                .manualCodeSigning(identity: "iPhone Developer", provisioningProfileSpecifier: "dev_forWildcard")
+                                .swiftActiveCompilationConditions("DEBUG")
+                        ),
+                        .release(
+                            name: "Inhouse",
+                            settings: SettingsDictionary()
+                                .manualCodeSigning(identity: "iPhone Distribution", provisioningProfileSpecifier: "dev_forWildcard")
+                                .swiftActiveCompilationConditions("INHOUSE")
+                        ),
+                        .release(
+                            name: "Release",
+                            settings: SettingsDictionary()
+                                .manualCodeSigning(identity: "iPhone Distribution", provisioningProfileSpecifier: "dev_forWildcard")
+                        )
+                    ])
+                )
+
+
+let project = Project(name: "CoffeeOverflow",
+                      organizationName: "com.Cashwalk",
+                      packages:
+                        [ .flexLayout,
+                          .pinLayout,
+                          .reactorKit,
+                          .rxSwift,
+                          .firebase],
+                        targets: [target])
+
+
+
---
+++ Tuist/ProjectDescriptionHelpers/Dependency/Dependencies.swift
@@ -0,0 +1,42 @@
+//
+//  Dependencies.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Nudge on 2023/01/18.
+//
+
+import ProjectDescription
+
+public extension TargetDependency {
+    static let flexLayout: TargetDependency = .package(product: "FlexLayout")
+    static let pinLayout: TargetDependency = .package(product: "PinLayout")
+    static let reactorKit: TargetDependency = .package(product: "ReactorKit")
+    static let rxSwift: TargetDependency = .package(product: "RxSwift")
+    static let firebase: TargetDependency = .package(product: "firebase-ios-sdk")
+    
+        
+}
+
+public extension Package {
+    static let flexLayout: Package = .remote(
+        url: "https://github.com/layoutBox/FlexLayout.git",
+        requirement: .upToNextMajor(from: "1.3.25")
+      )
+    static let pinLayout: Package = .remote(
+        url: "https://github.com/layoutBox/PinLayout.git",
+        requirement: .upToNextMajor(from: "1.10.0")
+      )
+    static let reactorKit: Package = .remote(
+        url: "https://github.com/ReactorKit/ReactorKit",
+        requirement: .upToNextMajor(from: "3.2.0")
+      )
+    static let rxSwift: Package = .remote(
+        url: "https://github.com/ReactiveX/RxSwift.git",
+        requirement: .upToNextMajor(from: "6.5.0")
+      )
+    static let firebase: Package = .remote(
+        url: "https://github.com/firebase/firebase-ios-sdk.git",
+        requirement: .upToNextMajor(from: "8.0.0")
+      )
+   
+}
