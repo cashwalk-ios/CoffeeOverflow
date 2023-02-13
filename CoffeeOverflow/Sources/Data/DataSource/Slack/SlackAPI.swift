85d0522def87625875c9a8a7860b49dc15481857
Martin <kim.suwan@cashwalk.io>
2023-02-13 17:21:45 +0900

    Add SlackAPI.swift

---
+++ CoffeeOverflow/Sources/Data/DataSource/Slack/SlackAPI.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+import Moya
+
+enum SlackAPI {
+    case lookupUserByEmail(email: String)
+    case postChatMessgae(channel: String, threadTS: String, text: String)
+    case fetchConversaionsReplies(channel: String, ts: String)
+}
+
+extension SlackAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "https://api.slack.com/methods")!
+        
+    }
+    
+    var path: String {
+        switch self {
+        case .lookupUserByEmail:
+            return "/users.lookupByEmail"
+        case .postChatMessgae:
+            return "/chat.postMessage"
+        case .fetchConversaionsReplies:
+            return "/conversations.replies"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .postChatMessgae:
+            return .post
+        default:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .lookupUserByEmail(let email):
+            return .requestParameters(
+                parameters: ["eamil": email],
+                encoding: URLEncoding.queryString
+            )
+        case .postChatMessgae(let channel, let threadTS, let text):
+            return .requestParameters(
+                parameters: [
+                    "channel": channel,
+                    "text": text,
+                    "thread_ts": threadTS
+                ],
+                encoding: URLEncoding.queryString
+            )
+        case .fetchConversaionsReplies(let channel, let ts):
+            return .requestParameters(
+                parameters: [
+                    "channel": channel,
+                    "ts": ts
+                ],
+                encoding: URLEncoding.queryString
+            )
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Authorization": "Bearer xoxb-1988873761187-4692591182289-vfvQSZelJ14svfkNIvcF7UGi"]
+    }
+
+}
