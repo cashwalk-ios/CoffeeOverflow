59dad4ff0b1fba8b29a4464735224990f2196be7
박성미 <ruysith@gmail.com>
2023-02-14 09:55:05 +0900

    Update 참가자 collectionView로 변경

---
+++ CoffeeOverflow/Sources/Presentation/Scene/MyQuestions/ParticipantsView.swift
@@ -0,0 +1,56 @@
+//
+//  ParticipantsView.swift
+//  CoffeeOverflow
+//
+//  Created by lovecat on 2023/02/14.
+//  Copyright © 2023 com.Cashwalk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FlexLayout
+import PinLayout
+
+class ParticipantsView: UIView {
+    fileprivate let rootFlexContainer = UIView()
+
+    private(set) var collectionView: UICollectionView
+    fileprivate let cellTemplate = ProfileCell()
+
+    fileprivate var profile: [Profile] = []
+    
+    init() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        super.init(frame: .zero)
+
+        collectionView.register(ProfileCell.self, forCellWithReuseIdentifier: ProfileCell.reuseIdentifier)
+        collectionView.backgroundColor = .clear
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = layout
+        
+        rootFlexContainer.flex.define { flex in
+            flex.addItem(collectionView).height(50)
+        }
+        addSubview(rootFlexContainer)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(profile: [Profile]) {
+        self.profile = profile
+        collectionView.reloadData()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        rootFlexContainer.pin.all(pin.safeArea)
+        rootFlexContainer.flex.layout()
+    }
+    
+}
+
+
