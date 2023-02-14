813b50e219f98e3740303c8305c07f3d890fb65b
최예주 <choi.yeaju@cashwalk.io>
2023-02-14 12:33:56 +0900

    Add GoogleSignin to Appdelegate

--- CoffeeOverflow/Sources/Application/AppDelegate.swift
+++ CoffeeOverflow/Sources/Application/AppDelegate.swift
@@ -1,11 +1,12 @@
 import UIKit
 import FirebaseCore
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     
     func application(
         _ application: UIApplication,
@@ -14,13 +15,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .white
-        window?.rootViewController = AppDependency.resolve().viewController
-        window?.makeKeyAndVisible()
-
-        return true
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in // 로그인 상태 복원
+            if error != nil || user == nil {
+                // Show the app's signed-out state.
+            } else {
+                // Show the app's signed-in state.
+            }
+        }
+            
+            window = UIWindow(frame: UIScreen.main.bounds)
+            let viewController = UIViewController()
+            viewController.view.backgroundColor = .white
+            window?.rootViewController = AppDependency.resolve().viewController
+            window?.makeKeyAndVisible()
+            
+            return true
+        }
+    
+    func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:] ) -> Bool {
+        var handled: Bool
+        
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        // Handle other custom URL types.
+        // If not handled by this app, return false.
+        return false
+    
     }
-
 }
+
